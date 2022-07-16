f57930cd8f841c15e4077244902e0d18d4eb856a
Pedro Petrini <pedropetrinicastello@gmail.com>
2022-07-16 10:45:30 -0300

    First Commit

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<header>
+        <a href='#' class='logo'>Petras<span>Gym</span></a>
+        <div class="toggle"><img src="./content/toggle.png"></div>
+</header>
+    <div class="banner">
+        <div class="textBox">
+            <h2>mais que saude !</h2>
+            <p>I is a long esttablished fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+            <a href="#">Ler Mais</a>
+        </div>
+        <div class="imgBox">
+            <img src="./content/man.png" alt="">
+        </div>
+    </div>
+<main>
